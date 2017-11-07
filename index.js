ce7e1806e4cd9fd56c9bbe5971017d03d541d594
Nathan Edwards <nathan@onedarnleyroad.com>
2017-11-07 10:30:26 +0000

    comment in index

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+// Here we might not want to do anything, OR we might want to bundle
+// all modules in one. Generally most modules will be required as needed
+// rather than a single script requiring everyhing.
+
+module.exports = {
+    // eg for debugging?
+    // myUtil: require('./myUtil'),
+};
